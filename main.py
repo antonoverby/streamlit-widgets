674d90411d8544d52e85e70dfd01bd50d5a4204b
Anton Overby <antonio.overby@theacsa.org>
2023-05-25 15:52:32 -0500

    more plotly vizzes

--- main.py
+++ main.py
@@ -2,7 +2,19 @@ import streamlit as st
 from st_pages import Page, Section, show_pages
 from app.utils import page_layout
 
+col1, col2, col3  = st.columns(3)
+
+with col1:
+    st.write("")
+
+with col2: 
+    st.image("assets/aclogotransparent.png", use_column_width=True)
+
+with col3:
+    st.write("")
 st.title("Welcome!")
+st.text("Here's a place for a homepage")
+
 
  # Structure of Pages on the left sidebar. Be careful b/c adjustments may take a server reset. Switching a Page to Section method seems to trigger this
 show_pages(
--- pages/plotlyviz.py
+++ pages/plotlyviz.py
@@ -1,22 +1,40 @@
 from app.utils import page_layout
 import streamlit as st
 import plotly.express as px
-from plotly.data import iris
+from plotly.data import iris, gapminder, stocks
+
 
 page_layout() 
 
-
+# Declaring variables from dataframe columns
 iris = iris()
 sepal_length = iris['sepal_length']
 petal_length = iris['petal_length']
 species = iris['species']
 
+gapminder = gapminder()
+country = gapminder['country']
+continent = gapminder['continent']
+year = gapminder['year']
+lifeExp = gapminder['lifeExp']
+pop = gapminder['pop']
+gdpPercap = gapminder['gdpPercap']
+iso_alpha = gapminder['iso_alpha']
+iso_num = gapminder['iso_num']
+
+stocks = stocks()
+date = stocks['date']
+goog = stocks['GOOG']
+aapl = stocks['AAPL']
+amzn = stocks['AMZN']
+fb = stocks['FB']
+nflx = stocks['NFLX']
+msft = stocks['MSFT']
 
 #Scatter
 st.header("Scatter")
 fig = px.scatter(iris, x=iris['petal_length'], y=iris['sepal_length'], color=iris['species'])
 st.plotly_chart(fig, use_container_width=True)
-
 
 #Bar
 st.header("Bar")
@@ -25,7 +43,10 @@ st.plotly_chart(fig, use_container_width=True)
 
 #Line
 st.header("Line")
-fig = px.line(iris, x=species, y=petal_length, color=iris['species'])
+fig = px.line(stocks,x=date, y=[goog, aapl, amzn, fb, nflx, msft])
 st.plotly_chart(fig, use_container_width=True)
 
-
+#Choropleth
+st.header("Choropleth")
+fig = px.choropleth(gapminder, locations=iso_alpha, color=lifeExp, hover_name=country)
+st.plotly_chart(fig, use_container_width=True)
