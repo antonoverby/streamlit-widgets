7e8bf857a7c3988da495139e397f2828b3a8c862
Anton Overby <aoverby.devworks@gmail.com>
2023-05-24 21:37:11 -0500

    added vizzes to plotly page, added a vizexplorer Jupyter notebook

--- pages/plotlyviz.py
+++ pages/plotlyviz.py
@@ -1,8 +1,31 @@
 from app.utils import page_layout
 import streamlit as st
 import plotly.express as px
+from plotly.data import iris
+
+page_layout() 
 
 
-page_layout()
+iris = iris()
+sepal_length = iris['sepal_length']
+petal_length = iris['petal_length']
+species = iris['species']
+
+
+#Scatter
+st.header("Scatter")
+fig = px.scatter(iris, x=iris['petal_length'], y=iris['sepal_length'], color=iris['species'])
+st.plotly_chart(fig, use_container_width=True)
+
+
+#Bar
+st.header("Bar")
+fig = px.bar(iris, x=sepal_length, y=species, color=iris['species'])
+st.plotly_chart(fig, use_container_width=True)
+
+#Line
+st.header("Line")
+fig = px.line(iris, x=species, y=petal_length, color=iris['species'])
+st.plotly_chart(fig, use_container_width=True)
 
 
